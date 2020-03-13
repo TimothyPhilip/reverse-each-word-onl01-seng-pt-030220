392d922f922e56e5621bd7769e3641b631b202d5
Philippe Timothe <philippetimothe@gmail.com>
2020-03-13 03:56:46 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,8 +1,7 @@
 def reverse_each_word(sentence1)
     new_sentence1 = [] << sentence1.split.reverse.join(" ")
     new_sentence1.collect do |s|
-      "#{s}".reverse >> [] = words
+      "#{s}".reverse
       
   end
-  words.toString()
 end
