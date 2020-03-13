73850dc08e806dc01dcda5b997195cd96ac6ac47
Philippe Timothe <philippetimothe@gmail.com>
2020-03-13 03:42:43 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,7 +1,7 @@
 def reverse_each_word(sentence1)
     new_sentence1 = [] << sentence1.split.reverse.join(" ")
     new_sentence1.collect do |s|
-      "#{s}".reverse.toString
+      "#{s}".reverse
       
   end
   
